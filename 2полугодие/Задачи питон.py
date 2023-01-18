81432c2126024a80b5e1b8513effe9089a31b39e
Averin973 <114554606+Averin973@users.noreply.github.com>
2023-01-18 11:53:27 +0700

    Update Задачи питон.py

--- 2полугодие/Задачи питон.py
+++ 2полугодие/Задачи питон.py
@@ -90,3 +90,15 @@ for x in range(16):
         goto(x*20,y*20)
         dot(5)
 done()
+
+
+    from turtle import *
+    left(90)
+    for i in range(7):
+        forward(300)
+        right(120)
+    pu()
+    for x in range(1,9):
+        for y in range(1,10):
+            goto(x*30,y*30)
+            dot(5)
